c8a8d0d990bc5028628c51860d714d5c006bd213
alramirez729 <alramirez729@gmail.com>
2024-09-20 15:47:52 -0400

    Styling changes in terms of color and added .css pages

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,14 +1,15 @@
 // src/Homepage.js
 import React from 'react';
+import './homepage.css';
 import homePageIcon from './images/carvcar.JPG'
 
 function Homepage() {
   return (
     <div>
       <div id="content">
-        <h1>Welcome to the Homepage</h1>
+        <h1>Welcome to Car vs. Car!</h1>
         <img src={homePageIcon} alt="CarVsCar" className="carvcar"></img>
-        <p>This is the homepage of CarVsCar Webapp.</p>
+        <p>Application that will make a comparison between different cars in order to determine which is best for a specific purpose.</p>
       </div>
     </div>
   );
