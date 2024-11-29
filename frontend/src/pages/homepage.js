ece3368a7c8ce27b63e396490e9825983a38e6b7
alramirez729 <alramirez729@gmail.com>
2024-11-29 09:28:34 -0500

    Homescreen title styling

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -4,8 +4,8 @@ import homePageIcon from './images/carvcar.JPG';
 
 function Homepage() {
   return (
-    <div className="my-20 flex rounded-lg flex-col items-center justify-center p-3 text-center font-mono">
-      <h1 className="text-4xl md:text-6xl lg:text-5xl font-bold text-gray-500 font-mono uppercase tracking-wide mb-5">
+    <div className="flex rounded-lg flex-col items-center justify-center p-3 text-center font-mono">
+      <h1 className="my-5 heading tracking-widest ring-1 ring-slate-300 bg-slate-200 rounded-l h-30 w-30 border-b-gray-300 border-2 p-5">
         Welcome to Car vs. Car!
       </h1>
       <img 
@@ -13,7 +13,7 @@ function Homepage() {
         alt="CarVsCar" 
         className="max-w-[80vw] md:max-w-[70vw] lg:max-w-[60vw] mb-5 border-4 border-gray-300 rounded-lg object-contain"
       />
-      <p className="text-base font-medium md:text-xl lg:text-2xl text-slate-900 font-mono leading-relaxed max-w-xl mx-auto px-5">
+      <p className="text-base font-medium md:text-xl lg:text-2xl text-slate-900 font-mono leading-relaxed max-w-xl mx-0 px-5">
         Can't decide between one car or the other? That's where we come in... Compare two distinct cars and see which one is right for you!
       </p>
       <Link 
